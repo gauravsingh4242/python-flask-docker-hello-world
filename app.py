fa29c78a055acdb1bb94a5de27f9c43fae72d78c
Gaurav Singh <gaurav.singh@rapyuta-robotics.com>
2018-12-05 15:53:01 +0530

    minor chnage

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 #!/bin/bash
-
 from flask import Flask
-
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return "Hello from dockerfile"
 
-
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
